af0a28ea741bcf769168d7072bc97322f461a1fe
Admin <someone@example.email>
2020-02-15 18:17:23 +0700

    Change year_of_birth field to dob with type DateTime

--- account/models.py
+++ account/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+import os
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete="CASCADE")
     phone = models.CharField(max_length = 18)
     room = models.CharField(max_length = 8)
-    year_of_birth = models.CharField(max_length = 4)
+    dob = models.DateTimeField(default = timezone.datetime.now())
     active = models.BooleanField(default = True)
-    avatar = models.ImageField(upload_to = 'images/')
+    avatar = models.ImageField(upload_to = 'images/', default = os.path.join(settings.STATIC_ROOT, 'avatar.png'))
